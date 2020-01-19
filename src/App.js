50e94cdfc892bfff380139a1efed2a5626bae167
karen <harutyunyank27@gmail.com>
2020-01-19 22:08:58 +0400

    styleing with variables

--- src/App.js
+++ src/App.js
@@ -7,6 +7,10 @@ const Blok = styled.div`
     ${ ({width})  => width ? `width: ${width};` : ''}
     ${ ({height})  => height ? `height: ${height};` : ''}
     ${ ({bgColor})  => bgColor ? `background-color: ${bgColor};` : ''}
+    ${ ({borderRadius}) => borderRadius ? `border-radius: ${borderRadius};` : ''}
+    ${ ({borderColor}) => borderColor ? `border: 1px solid ${borderColor};` : ''}
+    ${ ({position}) => position ? `position: ${position};` : ''}
+    ${ ({align}) => align ?  `text-align: ${align};` : '' }
 `;
 
 
@@ -18,10 +22,13 @@ const App = () => {
             <h1>Hello</h1>
             <Blok height='50px' bgColor="orange"></Blok>
             <Blok width="80px" height="100px" bgColor="violet"></Blok>
+            <Blok width='120px' height='65px' bgColor='yellow' borderRadius="4px 4px 4px 4px" borderColor="green"></Blok>
+            <Blok width="60px" height='80px' bgColor='green' position='relative'></Blok>
+            <Blok width="120px" height="95px" bgColor="black" align="center"></Blok>
+            
        </> 
     )
 };
 
 
-export default App;
-
+export default App
